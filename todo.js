087b923d581b638e2e233bc8fe353db9342bb2c0
Bruno Mateus <brunomateus@gmail.com>
2024-08-15 11:57:02 -0300

    todo componentizada

--- todo.js
+++ todo.js
@@ -1,28 +1,20 @@
 const { ref, computed, createApp } = Vue
 
-createApp({
+const app = createApp({
   setup() {
-    const categorias = ref([
-      'Estudo', 'Lazer', 'Trabalho', 'Leitura'
-    ])
     
   const tasks = ref([])
-
-  const inputDescription = ref('')
-  const selectCategoria = ref('')
-  const isEmpty = computed(() => inputDescription.value.length === 0 )
 
   const nConcluidas = computed(() => tasks.value.filter((item) => item.concluded === false))
   const concluidas = computed(() => tasks.value.filter((item) => item.concluded === true))
 
-  function addTask() {
+  function addTask(task) {
     tasks.value.push({
       id: tasks.value.length + 1,
-      description: inputDescription.value,
-      categoria: selectCategoria.value,
-      concluded: false
+      description: task.description,
+      categoria: task.categoria,
+      concluded: task.concluded
     })
-    inputDescription.value = ""
   }
 
   function remove(id) {
@@ -31,8 +23,88 @@ createApp({
       tasks.value.splice(pos, 1)
     }
   }
+
     return {
-      tasks, categorias, inputDescription, selectCategoria, isEmpty, concluidas, nConcluidas, addTask, remove
+      tasks, concluidas, nConcluidas, addTask, remove
     }
   }
-}).mount('#app')
+})
+
+app.component('task-form', {
+  emit: ['add'],
+  setup(_, { emit }) {
+    const categorias = ref([
+      'Estudo', 'Lazer', 'Trabalho', 'Leitura'
+    ])
+
+    const inputDescription = ref('')
+    const selectCategoria = ref('')
+    const isEmpty = computed(() => inputDescription.value.length === 0 )
+
+    function askToAdd() {
+      emit('add', {
+        description: inputDescription.value,
+        categoria: selectCategoria.value,
+        concluded: false
+      })
+
+      inputDescription.value = ""
+    }
+
+    return {
+      inputDescription, selectCategoria, isEmpty, categorias, askToAdd
+    }
+  },
+  template: `
+      <form @submit.prevent="askToAdd">
+      <label>
+        Descrição:
+        <input type="text" v-model="inputDescription"> 
+      </label>
+      <label>
+        Categoria:
+        <select v-model="selectCategoria">
+          <option v-for="cat in categorias" :value="cat">
+            {{ cat }}
+          </option>
+        </select> 
+      </label>
+      <input type="submit" :disabled="isEmpty">
+    </form>
+  `
+})
+
+app.component('task-list', {
+  props: ['description', 'empty', 'tasks'],
+  template: `
+    <div>
+      <h2> {{ description }} </h2>
+      <ul v-if="tasks.length > 0">
+        <slot></slot>
+      </ul>
+      <p v-else>
+        {{ empty }}
+      </p>
+    </div>
+  `
+})
+
+app.component('task-item', {
+  props: ['id', 'description', 'categoria', 'concluded'],
+  emit: ['delete', 'change'],
+  setup(props, { emit }){
+    function askToRemove() {
+      emit('delete', props.id)
+    }
+    
+    return { askToRemove }
+  },
+  template: `
+  <li>
+    {{ description }} - {{ categoria }}
+    <input type="checkbox" @change="concluded = !concluded" :checked="concluded">
+    <button @click="askToRemove"> Remover </button>
+  </li>`
+})
+
+app.mount('#app')
